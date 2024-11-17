92ce4c8451d0d5e51bb6eae946a109fb91325d07
Alim <rizaev.alim06@mail.ru>
2024-11-17 17:10:14 +0300

    tc_4_4

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
+app.engine('ejs',require('ejs-locals'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -31,7 +32,7 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error');res.render('error',{title: 'Exchange'}); 
 });
 
 module.exports = app;
