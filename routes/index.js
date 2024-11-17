9a94e13b53e00a0588be38d886d3f287656f2835
Alim <rizaev.alim06@mail.ru>
2024-11-17 16:05:33 +0300

    tc_4_3

--- routes/index.js
+++ routes/index.js
@@ -7,18 +7,31 @@ router.get('/', function(req, res, next) {
 });
 
 /* Страница Bybit */
-router.get('/Bybit', function(req, res, next) {
-  res.send("<h1>Страница Bybit</h1>");
-});
-
-/* Страница Binance */
-router.get('/Binance', function(req, res, next) {
-  res.send('<h1>Страница Binance</h1>');
+router.get('/bybit', function(req, res, next) {
+  res.render('exchange', {
+    title: "Bybit",
+    picture: "images/bybit.png",
+    desc: "Bybit — это криптовалютная биржа, которая позволяет трейдерам торговать различными криптовалютами. Биржа известна своими низкими комиссиями и продвинутыми функциями для профессионалов."
+  });
 });
 
 /* Страница Houbi */
-router.get('/Houbi', function(req, res, next) {
-  res.send('<h1>Страница Houbi</h1>');
+router.get('/houbi', function(req, res, next) {
+  res.render('exchange', {
+    title: "Houbi",
+    picture: "images/houbi.png",  // Путь к картинке Houbi
+    desc: "Houbi — это криптовалютная биржа, предоставляющая множество возможностей для трейдинга. Она известна своей надежностью и большим выбором цифровых активов."
+  });
 });
+
+/* Страница Binance */
+router.get('/binance', function(req, res, next) {
+  res.render('exchange', {
+    title: "Binance",
+    picture: "images/binance.png",  // Путь к картинке Binance
+    desc: "Binance — крупнейшая криптовалютная биржа в мире, предоставляющая услуги трейдинга и стейкинга, а также доступ к множеству криптовалютных продуктов."
+  });
+});
+
 
 module.exports = router;
