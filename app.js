39b50bc9f817adf82604e5348f09575819270c2c
Alim <rizaev.alim06@mail.ru>
2024-11-17 14:55:18 +0300

    Пункт 4_2

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -29,11 +28,8 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* Страница Bybit */
+router.get('/Bybit', function(req, res, next) {
+  res.send("<h1>Страница Bybit</h1>");
+});
+
+/* Страница Binance */
+router.get('/Binance', function(req, res, next) {
+  res.send('<h1>Страница Binance</h1>');
+});
+
+/* Страница Houbi */
+router.get('/Houbi', function(req, res, next) {
+  res.send('<h1>Страница Houbi</h1>');
+});
+
 module.exports = router;
