eb79afb8196356a8464edfb0e04ea2b65897bce2
Alim <rizaev.alim06@mail.ru>
2024-11-21 19:12:39 +0300

    tc_6_2

--- createDB.js
+++ createDB.js
@@ -1,17 +1,21 @@
 const { MongoClient } = require('mongodb');
 // or as an es module:
 // import { MongoClient } from 'mongodb'
+var data = require("./data.js").data;
+console.log(data);
 
 // Connection URL
-const url = 'mongodb://localhost:27017'; const client = new MongoClient(url);
+const url = 'mongodb://localhost:27017'; 
+const client = new MongoClient(url);
  
 // Database Name
-const dbName = 't';
-
+const dbName = 'test2024';
 
 async function main() {
 // Use connect method to connect to the server await client.connect();
-console.log('Connected successfully to server'); const db = client.db(dbName);
+console.log('Connected successfully to server'); 
+
+const db = client.db(dbName);
 const collection = db.collection('documents');
 
 // the following code examples can be pasted here...
