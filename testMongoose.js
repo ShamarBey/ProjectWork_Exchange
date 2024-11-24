6d97d628dc527c0f8e781185fce259c4c39f7b5e
Alim <rizaev.alim06@mail.ru>
2024-11-24 22:30:49 +0300

    tc_7_2

--- testMongoose.js
+++ testMongoose.js
@@ -1,7 +1,17 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/testMongoose2024');
 
-const Exchange = mongoose.model('Exchange', { name: String });
+// Определяем схему для модели
+var schema = mongoose.Schema({ name: String });
 
+// Добавляем метод к схеме
+schema.methods.announce = function() {
+  console.log(this.name + " объявили о торгах");
+};
+
+// Создаем модель на основе схемы
+const Exchange = mongoose.model('Exchange', schema);
+
+// Создаем экземпляр модели и сохраняем его в базе данных
 const exchange = new Exchange({ name: 'Биржи' });
-exchange.save().then(() => console.log('Биржи = P2P'));
+exchange.save().then(() => exchange.announce());
