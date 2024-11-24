d27ba818dee821f87cfc396ac031eef9e1994cc4
Alim <rizaev.alim06@mail.ru>
2024-11-24 23:27:36 +0300

    tc_8_2

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ mongoose.connect('mongodb://localhost/tc2024')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var exchanges = require('./routes/exchanges');
 
 var app = express();
 
@@ -23,6 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/exchanges', exchanges);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
