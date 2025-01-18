b8447857d2a6a6cb4e797f568930185c121d5bfe
Alim <rizaev.alim06@mail.ru>
2025-01-19 00:17:18 +0300

    tc_9_5

--- app.js
+++ app.js
@@ -34,12 +34,11 @@ app.use(session({
   store: MongoStore.create({ mongoUrl: 'mongodb://localhost/tc2024' })
   }))
   
+app.use(function(req,res,next){
+  req.session.counter = req.session.counter + 1 || 1
+  next()
+  })
 
-  app.use(function(req,res,next){
-    req.session.counter = req.session.counter + 1 || 1
-    next()
-    })
-    
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
