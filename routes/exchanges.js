e859db2873239ea932ac8bf1d99182dd3f19695c
Alim <rizaev.alim06@mail.ru>
2025-01-19 22:09:07 +0300

    tc_10_8

--- routes/exchanges.js
+++ routes/exchanges.js
@@ -1,29 +1,29 @@
 var express = require('express');
-const { Exchange } = require('../models/exchange');
 var router = express.Router();
-var exchanges = require('../models/exchange').exchanges;
+var Cat = require('../models/exchange').exchange;
+var checkAuth = require("../middlewares/checkAuth.js");
 
-/* GET users listing. */
+
+/* GET exchange listing. */
 router.get('/', function(req, res, next) {
-  res.send('Новый маршрутизатор, для маршрутов, начинающихся с exchanges');
+  res.send('Новый маршрутизатор, для маршрутов, начинающихся с exchange');
 });
 
-//  Страница после слеша /exchanges/....
-router.get("/:nick", function(req, res, next) {
-    res.send(req.params.nick);
-});
-
-
-router.get("/:nick", async function(req, res, next) {
+/* Страница котов */
+router.get("/:nick", checkAuth, async function(req, res, next) {
     var exchanges = await exchange.find({nick: req.params.nick});
     console.log(exchanges)
-    if(!exchanges.length) return next(new Error("Нет такой модели, в разработке."))
+    if(!exchanges.length) return next(new Error("Нет такой биржи в мировой экономике"))
         var exchange = exchanges[0];
-        res.render('exchanges', {
+        res.render('exchange', {
             title: exchange.title,
             picture: exchange.avatar,
             desc: exchange.desc
-        }) 
+        })
 });
+
+
+//Hero.find({},{_id:0,title:1,nick:1},callback)
+
 
 module.exports = router;
