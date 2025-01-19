2780dde9d3619ce9d706263c1744dae13687a487
Alim <rizaev.alim06@mail.ru>
2025-01-19 12:47:32 +0300

    tc_10_2

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,27 @@
-
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  req.session.greeting = "Hi!!!!";
-  res.render('index', { title: 'Express', counter:req.session.counter  });
+  req.session.greeting = "Hi!!!";
+  res.render('index', { title: 'Express',  counter:req.session.counter  });
 });
- 
+
 /* GET login/registration page. */
 router.get('/logreg', function(req, res, next) {
   res.render('logreg',{title: 'Вход'});
   });
+
+
+  /* POST login/registration page. */
+router.post('/logreg', function(req, res, next) {
+   var username = req.body.username
+   var password = req.body.password
+   console.log(username);
+   console.log(password);
+});
+
+    
+
 
 module.exports = router;
