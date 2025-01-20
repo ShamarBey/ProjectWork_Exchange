4a369677bfbf8bc33d93ddad976dc7776a2b9782
Alim <rizaev.alim06@mail.ru>
2025-01-20 08:27:31 +0300

    tc_10_8

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/tc2024')
-var session = require("express-session")
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/tc2024');
+var session = require("express-session");
 var MongoStore = require('connect-mongo');
 
 var indexRouter = require('./routes/index');
@@ -15,7 +15,7 @@ var exchanges = require("./routes/exchanges");
 var app = express();
 
 // view engine setup
-app.engine('ejs',require('ejs-locals')); 
+app.engine('ejs', require('ejs-locals'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -23,33 +23,33 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'))); // Обработка статических файлов
 
+// Настройка сессии
 app.use(session({
   secret: "Exchanges",
-  cookie:{maxAge:60*1000},
+  cookie: { maxAge: 60 * 1000 },
   proxy: true,
   resave: true,
   saveUninitialized: true,
-  store: MongoStore.create({mongoUrl:
-  'mongodb://localhost/tc2024'})
-    
-  }))
+  store: MongoStore.create({ mongoUrl: 'mongodb://localhost/tc2024' })
+}));
 
-app.use(function(req,res,next){
-  req.session.counter = req.session.counter + 1 || 1
-  next()
-  })
-app.use(require("./middlewares/createMenu.js"))
-app.use(require("./middlewares/createUser.js"))
+// Middleware для увеличения счетчика
+app.use(function(req, res, next) {
+  // Увеличиваем счетчик только для HTML-страниц
+  if (req.path === '/' || req.path.endsWith('.html')) {
+    req.session.counter = req.session.counter + 1 || 1;
+  }
+  next();
+});
+
+app.use(require("./middlewares/createMenu.js"));
+app.use(require("./middlewares/createUser.js"));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/exchanges', exchanges);
-
-// app.use('/', exchanges);
-// app.use('/exchanges', exchanges);
-// app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -64,7 +64,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');res.render('error',{title: 'Exchange'}); 
+  res.render('error', { title: 'Exchange' });
 });
 
 module.exports = app;
